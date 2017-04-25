49a5ea20f0d42ffab8ae65e98bc6373287af87ad
canlas1 <dc@uscr.com>
2017-04-25 11:13:05 -0400

    Worked on BasicCard creation, and put into an array

--- BasicCard.js
+++ BasicCard.js
@@ -1,161 +1,105 @@
 console.log("this is loaded!");
+//##########################################################
+var fs = require('fs');
+var inquirer = require('inquirer');
+var request = require('request');
+//##########################################################
+//GLOBAL VARIABLES
+var name;
+var nodeArg;
+var emptyArray = []
+    //##########################################################
+    //FUNCTIONS I WANT
+
+startGame();
+
+function greeting(name) {
+    console.log("------------------------");
+    console.log("------------------------");
+    console.log("Name:  " + name);
+}
+
+function startGame() {
+    console.log("------------------------");
+    inquirer.prompt([{
+        type: "confirm",
+        message: "Do You Want to Play Basic FLashCards?",
+        name: "confirm"
+    }, {
+        type: "input",
+        message: "What is your name?",
+        name: "name"
+    }]).then(function(answer) {
+        greeting(answer.name)
+        if (answer.confirm) {
+            createFlashCard();
+        } else {
+            console.log("Then Dont Play!")
+        }
+    });
+};
+
+//##########################################################
 //creating a constructor function for the 2 arguments question and answer
-var basicCard = function (question, answer) {
-	//this refers to basicCard, question refers to the first argument
-	this.question = question;
-	//this refers to basicCard, answer refers to the second argument
-	this.answer = answer;
-	//this refers to basicCard, seeFrontOfCard is a passed in method to read the front of the card which is the question
-	this.seeFrontOfCard = function() {
-		console.log(question);
-	}
-	//this refers to basicCard, seeFrontOfCard is a passed in method to read the back of the card which is the answer
-	this.seeBackOfCard = function() {
-		console.log(answer);
-	}
-}
-	//new constructor based on the first constructor function.  Note there are preset arguments
-var playCards = new basicCard("Who was the first president of the United States?", "George Washington");
+function createFlashCard() {
+    inquirer.prompt([{
+        type: 'confirm',
+        message: "Do you want to create a new flashcard?",
+        name: "makeAFlashCard"
+    }]).then(function(answer) {
+            if (answer.makeAFlashCard) {
+                inquirer.prompt([{
+                    type: "input",
+                    message: "Write a question",
+                    name: "question"
+                }, {
+                    type: "input",
+                    message: "What is the answer",
+                    name: "answer"
+                }]).then(function(answer) {
+                    var newCard = new basicCard(answer.question, answer.answer);
+                    console.log(newCard);
+                    emptyArray.push(newCard);
+                    createFlashCard();
+                })
+            } else {
+                inquirer.prompt([{
+                    type: 'confirm',
+                    message: "Do you want to play the card game?",
+                    name: "cardGameConfirm"
+                }]).then(function(answer) {
+                    if (answer.cardGameConfirm) {
+                        playGame()
+                    }
+                })
+            }
+        });
+    };
 
-var nodeArg= process.argv;
-var userInput = process.argv[2];
-if (userInput == "yes") {
-	playCards.seeFrontOfCard();
-	playCards.seeBackOfCard();
-}
+    function playGame() {
+        console.log("We are going to start playing the game");
+        console.log(emptyArray);
+        // body...
+    }
 
-// allows to export the constructor basicCard	
-module.exports = basicCard;
-
-// // 
-// var firstPresident = new BasicCard(
-//     "Who was the first president of the United States?", "George Washington");
-
-// // "Who was the first president of the United States?"
-// console.log(firstPresident.front); 
-
-// // "George Washington"
-// console.log(firstPresident.back); 
-
-// var firstPresidentCloze = new ClozeCard(
-//     "George Washington was the first president of the United States.", "George Washington");
-
-// // "George Washington"
-// console.log(firstPresidentCloze.cloze); 
-
-// // " ... was the first president of the United States.
-// console.log(firstPresidentCloze.partial); 
-
-// // "George Washington was the first president of the United States.
-// console.log(firstPresidentCloze.fullText)
-
-// // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
-// var brokenCloze("This doesn't work", "oops"); 
-// //
+    var basicCard = function(question, answer) {
+        //this refers to basicCard, question refers to the first argument
+        this.question = question;
+        //this refers to basicCard, answer refers to the second argument
+        this.answer = answer;
+        //this refers to basicCard, seeFrontOfCard is a passed in method to read the front of the card which is the question
 
 
+        function seeFrontOfCard() {
+            console.log(this.question)
+        }
+
+        function seeBackOfCard() {
+            console.log(this.answer);
+        }
+        // inquirer.prompt(seeFrontOfCard, seeBackOfCard);
+    }
 
 
-// var BasicCard = function() {
-//   this.question = "Who was the first president of the United States?";
-//   this.answer = "George Washington";
-  
-
-//   // method which feeds the digitalpal when they are hungry and sets them to sleepy
-//   this.question = function() {
-//     if (this.question === true) {
-//       console.log("That was yummy!");
-//       this.hungry = false;
-//       this.sleepy = true;
-//     }
-//     else {
-//       console.log("No thanks, I'm full.");
-//     }
-//   };
-
-//   // method which puts the digitalpal to sleep when they are sleepy, increases their age by one,
-//   // and sets them to bored
-//   this.sleep = function() {
-//     if (this.sleepy === true) {
-//       console.log("ZZzzZZZzzzZZz~~");
-//       this.sleepy = false;
-//       this.bored = true;
-//       this.increaseAge();
-//     }
-//     else {
-//       console.log("No way! I'm not tired!");
-//     }
-//   };
-
-//   // method which allows the user to play with their digitalpal when they are bored and sets them to hungry
-//   this.play = function() {
-//     if (this.bored === true) {
-//       console.log("Yay! Let's play!");
-//       this.bored = false;
-//       this.hungry = true;
-//     }
-//     else {
-//       console.log("Not right now. Maybe later?");
-//     }
-//   };
-
-//   // method which is called in "this.sleep" to increase the age of the digitalpal by one
-//   this.increaseAge = function() {
-//     this.age++;
-//     console.log("Happy Birthday to me! I am " + this.age + " old!");
-//   };
-// };
-
-
-
-
-
-
-
-
-// // here's your scope-safe constructor:
-// function President(title) {
-//   if (this instanceof Book) {
-//     this.title  = title;
-//   } else {
-//     return new Book(title);
-//   }
-// }
-
-// // and here's a nice thing you can do with scope-safe constructors:
-// var books = [
-//   'JavaScript: The Definitive Guide',
-//   'Professional JavaScript for Web Developers',
-//   'JavaScript: The Good Parts',
-//   'JavaScript Patterns',
-//   'Developing Backbone.js Applications'
-//   // etc.
-// ];
-
-// // books.map(Book);
-
-
-// function Person(name, age, job){    
-//   this.name = name;    
-//   this.age = age;    
-//   this.job = job; 
-// }                   
-// var person = new Person("Nicholas", 29, "Software Engineer");
-// alert(person.name); //Nicholas
-// alert(person.age);//29
-// alert(person.job);//Software Engineer
-
-// function Person(name, age, job){    
-//   if (this instanceof Person){        
-//     this.name = name;        
-//     this.age = age;        
-//     this.job = job;    
-//   } else {        
-//     return new Person(name, age, job);    
-//   } 
-// }                   
-// var person1 = Person("Nicholas", 29, "Software Engineer"); 
-// console.log(person1);   //"" 
-// alert(person1.name);  //"Nicholas"
-// var person2 = new Person("Shelby", 34, "Ergonomist"); 
-// alert(person2.name);  //"Shelby"
+    //new constructor based on the first constructor function.  Note there are preset arguments
+    var playCards = new basicCard("Who was the first president of the United States?", "George Washington");
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,35 +1,175 @@
 console.log('this is loaded!');
 
-// ClozeCard` should have a property or method that contains or returns _only_ the cloze-deleted portion of the text.
-
-// ClozeCard` should have a property or method that contains or returns _only_ the partial text.
-
-// ClozeCard` should have a property or method that contains or returns _only_ the full text.
-
-// ClozeCard` should throw or log an error when the cloze deletion does _not_ appear in the input text.
+// dependency for inquirer npm package
+var inquirer = require("inquirer");
 
 
-var clozeCard = function(statement, clozeSelection) {
-    this.statement = statement;
-    this.clozeSelection = clozeSelection;
-	    //refered to w3 schools
-	    var newStatement = statement.replace(/clozeSelection/g, "_____________");
-    this.seeFrontOfCard = function() {
-        console.log(newStatement);
-    }
-    this.seeBackOfCard = function() {
-        console.log(clozeSelection);
+// "George Washington"
+// console.log(firstPresidentCloze.cloze); 
+
+// // " ... was the first president of the United States.
+// console.log(firstPresidentCloze.partial); 
+
+// // "George Washington was the first president of the United States.
+// console.log(firstPresidentCloze.fullText);
+
+var ClozeCard = function(text, cloze) {
+    this.text = text;
+    this.cloze = cloze;
+    this.clozeStatement = "[president_name] was the [president_number] president of the United States";
+
+    var president_names = [];
+    var president_number = [];
+    var loop = 0;
+
+    var getPresident = function(loop) {
+        console.log(loop);
+        console.log(president_name);
+
+        if (loop < 2) {
+            inquirer.prompt({
+                name: "president_name",
+                message: "Enter a Presidents Name: "
+            }).then(function(answers) {
+                president_names.push(anwers.president_name);
+                loop++
+                getPresident();
+            });
+        }
+        getPresident();
     }
 }
 
-var presidentQuestion = new clozeCard("George Washington was the first president of the United States.", "George Washington")
-
-var userInput = process.argv[2];
-
-if (userInput == "yes") {
-    presidentQuestion.seeFrontOfCard();
-    presidentQuestion.seeBackOfCard();
-}
 
 
-module.exports = clozeCard; 
+
+
+
+//   this.front = function() {
+//     if (this.front === this.text) {
+//       console.log("That was yummy!");
+//       this.hungry = false;
+//       this.sleepy = true;
+//     }
+//     else {
+//       console.log("No thanks, I'm full.");
+//     }
+//   };
+
+//    {
+//     //not inside so we need to reference th
+//     console.log("THIS IS THE CLOZE TEXT:  " + this.text);
+
+//     var newText = this.text.replace(this.cloze, "_____________");
+//     console.log(newText);
+//     }
+
+//     this.seeBackOfCard = function() {
+//     console.log("THIS IS THE CLOZE ANSWER" + this.cloze);
+//     };
+// };
+
+
+
+// // var firstPresidentCloze = new ClozeCard(
+// //     "George Washington was the first president of the United States.", "George Washington");
+
+
+
+
+
+// var count = 0;
+// var newArr = [];
+
+// // var newFlashCards = function() {
+
+//     // if (count < 2) {
+//     //     // for (var i = 0; i<)
+//         inquirer.prompt([
+//           {
+//             name: "name",
+//             message: "what is your name?"
+//         }, {
+//             title: "title",
+//             message: "what is your title?"
+//         }, {
+//             age: "age",
+//             message: "how old are you?"
+//         }, {
+//             favoriteLanguage: "language",
+//             message: "What is your favorite language?"
+
+//         }]).then(function(answers) {
+//             console.log(answers)
+//             var newFlashCards = new ClozeCards(answers.text, answers.cloze);
+//             console.log(newFlashCards);
+//           });
+
+
+//       if (count.length < 5) {
+//   starters.push(player);
+//   team.push(player);
+//   console.log(player.name + " added to the starters");
+// }
+// else {
+//   subs.push(player);
+//   team.push(player);
+//   console.log(player.name + " added to the subs");
+// }
+//       newArr.push(newFlashCards);
+
+//       newFlashCards.seeFrontOfCard();
+
+//       count++;
+
+//       newFlashCards();
+//   });
+
+
+// console.log('All questions asked');
+
+// };
+
+// console.log(newFlashCards);
+
+
+// // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
+// var BrokenCloze = new ClozeCard("This doesn't work", "oops");
+
+
+// ClozeCard.prototype.seeFrontOfCard = function() {
+//     //not inside so we need to reference th
+//     console.log(this.text);
+//     var newText = this.text.replace(this.cloze, "_____________");
+//     console.log(newText);
+// }
+
+// ClozeCard.prototype.seeBackOfCard = function() {
+//     console.log(this.clozeSelection);
+// }
+
+// var question1 = new ClozeCard("George Washington was the first president of the United States.", "George Washington")
+
+
+// if (userInput == "yes") {
+//     PresidentQuestion.seeFrontOfCard();
+//     PresidentQuestion.seeBackOfCard();
+// }
+
+
+
+// function makeABasicFlashCard(front, back) {
+//     // blah blah blah do something with front
+//     // blah blah blah do something with back
+//     // fs.write("textfilename", this.front + this.back)
+//     makeAClozeCard();
+// }
+
+// function makeAClozeCard(front, back) {
+//     // blah blah blah do something with front
+//     // blah blah blah do something with back
+//     // fs.write("someothertextfilename", this.front + this.back)
+//     greeting();
+// }
+
+// // module.exports = ClozeCard;
