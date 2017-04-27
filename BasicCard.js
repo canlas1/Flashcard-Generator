a2669d71318fea08d40185aad5f0054773f63c8b
canlas1 <dc@uscr.com>
2017-04-27 01:01:34 -0400

    Committing Basic and Cloze Prompts

--- BasicCard.js
+++ BasicCard.js
@@ -2,104 +2,59 @@ console.log("this is loaded!");
 //##########################################################
 var fs = require('fs');
 var inquirer = require('inquirer');
-var request = require('request');
-//##########################################################
-//GLOBAL VARIABLES
-var name;
-var nodeArg;
-var emptyArray = []
-    //##########################################################
-    //FUNCTIONS I WANT
 
-startGame();
+//creating a constructor function for the 2 arguments question and answer
+function BasicCard(question, answer) {
+    this.question = question;
+    this.answer = answer;
 
-function greeting(name) {
-    console.log("------------------------");
-    console.log("------------------------");
-    console.log("Name:  " + name);
-}
+    this.displaycardContent = function() {
+        console.log("Front: " + this.question + " " + "Back: " + this.answer);
+    }
 
-function startGame() {
-    console.log("------------------------");
-    inquirer.prompt([{
-        type: "confirm",
-        message: "Do You Want to Play Basic FLashCards?",
-        name: "confirm"
-    }, {
-        type: "input",
-        message: "What is your name?",
-        name: "name"
-    }]).then(function(answer) {
-        greeting(answer.name)
-        if (answer.confirm) {
-            createFlashCard();
-        } else {
-            console.log("Then Dont Play!")
-        }
-    });
+    this.printCardInfo = function() {
+        var printCard = "Front: " + this.question + "\nBack: " + this.answer + "\r\n";
+
+
+        fs.appendFileSync("basicLog.txt", printCard);
+    }
 };
 
-//##########################################################
-//creating a constructor function for the 2 arguments question and answer
-function createFlashCard() {
-    inquirer.prompt([{
-        type: 'confirm',
-        message: "Do you want to create a new flashcard?",
-        name: "makeAFlashCard"
+function createBasicCard() {
+    inquirer.prompt([
+
+
+     {
+        name: "question",
+        message: "Enter Question of Basic Flash Card: "
+    }, {
+        name: "answer",
+        message: "Enter Answer of Basic Flash Card:  "
+
+    }, {
+        type: "confirm",
+        message: "Do you want to create another Card?",
+        name: "continue",
+        default: true
+       
+
+
     }]).then(function(answer) {
-            if (answer.makeAFlashCard) {
-                inquirer.prompt([{
-                    type: "input",
-                    message: "Write a question",
-                    name: "question"
-                }, {
-                    type: "input",
-                    message: "What is the answer",
-                    name: "answer"
-                }]).then(function(answer) {
-                    var newCard = new basicCard(answer.question, answer.answer);
-                    console.log(newCard);
-                    emptyArray.push(newCard);
-                    createFlashCard();
-                })
-            } else {
-                inquirer.prompt([{
-                    type: 'confirm',
-                    message: "Do you want to play the card game?",
-                    name: "cardGameConfirm"
-                }]).then(function(answer) {
-                    if (answer.cardGameConfirm) {
-                        playGame()
-                    }
-                })
-            }
-        });
-    };
-
-    function playGame() {
-        console.log("We are going to start playing the game");
-        console.log(emptyArray);
-        // body...
-    }
-
-    var basicCard = function(question, answer) {
-        //this refers to basicCard, question refers to the first argument
-        this.question = question;
-        //this refers to basicCard, answer refers to the second argument
-        this.answer = answer;
-        //this refers to basicCard, seeFrontOfCard is a passed in method to read the front of the card which is the question
-
-
-        function seeFrontOfCard() {
-            console.log(this.question)
+        var newCard = new BasicCard(answer.question, answer.answer);
+        newCard.printCardInfo();
+        if (answer.continue === true){
+            createBasicCard()
+        }
+        else{
+            console.log("see basicLog.txt for new flashcards!")
         }
 
-        function seeBackOfCard() {
-            console.log(this.answer);
-        }
-        // inquirer.prompt(seeFrontOfCard, seeBackOfCard);
-    }
+
+    });
+}
+
+   createBasicCard();
+
+module.exports = BasicCard;
 
 
-    //new constructor based on the first constructor function.  Note there are preset arguments
-    var playCards = new basicCard("Who was the first president of the United States?", "George Washington");
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,175 +1,243 @@
-console.log('this is loaded!');
+// console.log('this is loaded!');
 
-// dependency for inquirer npm package
+var fs = require("fs");
 var inquirer = require("inquirer");
 
+var ClozeCard = function(statement, cloze, partStatement) {
+    this.statement = statement;
+    this.cloze = cloze;
 
-// "George Washington"
-// console.log(firstPresidentCloze.cloze); 
 
-// // " ... was the first president of the United States.
-// console.log(firstPresidentCloze.partial); 
+    this.partStatement = function() {
+        var cloze = this.statement.replace(this.cloze, "..........");
+        
+        return cloze;
+     }
 
-// // "George Washington was the first president of the United States.
-// console.log(firstPresidentCloze.fullText);
+     this.printCardInfo = function () {
+        var printCard = "Front: " + this.statement + "\nBack: " + this.cloze + "\r\n";  
 
-var ClozeCard = function(text, cloze) {
+        fs.appendFileSync("clozeLog.txt", printCard)
+     }
+ };
+
+function createClozeCard() {
+    inquirer.prompt([
+    {
+        name: "statement", 
+        message: "Enter the the statement for the Cloze Card:"
+    }, {
+        name: "cloze", 
+        message: "Enter the CLOZE portion of the statement: "
+    }, {
+        type: "confirm", 
+        message: "Do you want to create another Cloze Card?", 
+        name: "continue", 
+        default: true
+    }]).then(function(answers){
+        var newCard = new ClozeCard(answers.statement, answers.cloze);
+        newCard.printCardInfo();
+        if (answers.continue === true) {
+            createClozeCard()
+        }
+        else{
+            console.log("see clozeLog.txt for new flashcards!");
+        }
+    });    // body...
+};
+createClozeCard();
+
+
+//     // this.clozeStatement = "[president_name] was the [president_number] president of the United States";
+
+//     var president_names = [];
+//     var president_number = [];
+//     var loop = 0;
+
+//     var getPresident = function(loop) {
+//         console.log(loop);
+//         console.log(president_name);
+
+//         if (loop < 2) {
+//             inquirer.prompt({
+//                 name: "president_name",
+//                 message: "Enter a Presidents Name: "
+//             }).then(function(answers) {
+//                 president_names.push(anwers.president_name);
+//                 loop++
+//                 getPresident();
+//             });
+//         }
+//         getPresident();
+//     }
+// }
+
+
+// function ClozeCard(text, cloze) {
+//     this.cloze = cloze;
+//     this.text = text;
+//     this.displayCardInfo = function() {
+//         console.log("Cloze: " + this.cloze + " " + "Text: " + this.cardText);
+//     }
+
+//     this.writeCardInfo = function() {
+//         var writeCard = this.cardWhole + "\r\n";
+
+//         fs.appendFile("cloze.txt", writeCard);
+//     }
+
+//     this.displayClozeDeleted = function() {
+//         var cloze = this.cardWhole.replace(this.cardCloze, "...");
+//         return cloze;
+//     }
+// };
+
+// function createCard() {
+//     inquirer.prompt([{
+//         name: "cloze",
+//         message: "Enter the cloze part of the flashcard: "
+//     }, {
+//         name: "text",
+//         message: "Enter the whole text of the flashcard: "
+//     }, {
+//         type: "confirm",
+//         message: "Display cloze deleted flashcard?",
+//         name: "display",
+//         default: true
+//     }, {
+//         type: "confirm",
+//         message: "Do you wish to add another cloze flashcard?",
+//         name: "continue",
+//         default: true
+//     }]).then(function(answers) {
+//         var card = new ClozeFlashCard(answers.text, answers.cloze);
+//         card.writeCardInfo();
+
+//         if (answers.display === true)
+//             console.log(card.displayClozeDeleted());
+
+//         if (answers.continue === true)
+//             createCard();
+//         else
+//             console.log("Please see cloze.txt for your newly created flashcards.")
+//     });
+// }
+
+// createCard();
+// //##########################################################
+// var fs = require('fs');
+// var inquirer = require('inquirer');
+// var request = require('request');
+// //##########################################################
+// //GLOBAL VARIABLES
+// var name;
+// var nodeArg;
+// var emptyArray = []
+//     //##########################################################
+//     //FUNCTIONS I WANT
+
+// startGame();
+
+// function greeting(name) {
+//     console.log("------------------------");
+//     console.log("------------------------");
+//     console.log("Name:  " + name);
+// }
+
+// function startGame() {
+//     console.log("------------------------");
+//     inquirer.prompt([{
+//         type: "confirm",
+//         message: "Do You Want to Play Cloze Card Game?",
+//         name: "confirm"
+//     }, {
+//         type: "input",
+//         message: "What is your name?",
+//         name: "name"
+//     }]).then(function(answer) {
+//         greeting(answer.name)
+//         if (answer.confirm) {
+//             createFlashCard();
+//         } else {
+//             console.log("Then Dont Play!")
+//         }
+//     });
+// };
+
+// //##########################################################
+// //creating a constructor function for the 2 arguments question and answer
+// function createFlashCard() {
+//     inquirer.prompt([{
+//         type: 'confirm',
+//         message: "Do you want to create a new flashcard?",
+//         name: "makeAFlashCard"
+//     }]).then(function(answer) {
+//             if (answer.makeAFlashCard) {
+//                 inquirer.prompt([{
+//                     type: "input",
+//                     message: "Write a statement",
+//                     name: "question"
+//                 }, 
+
+//                 {
+//                     type: "input",
+//                     message: "Choose a Cloze",
+//                     name: "cloze"
+//                 }
+
+
+//                 ]).then(function(answer) {
+//                     var newCard = new clozeCard(answer.question, answer.answer, cloze.answer);
+//                     console.log(newCard);
+//                     emptyArray.push(newCard);
+//                     createFlashCard();
+//                 })
+//             } else {
+//                 inquirer.prompt([{
+//                     type: 'confirm',
+//                     message: "Do you want to play the card game?",
+//                     name: "cardGameConfirm"
+//                 }]).then(function(answer) {
+//                     if (answer.cardGameConfirm) {
+//                         playGame()
+//                     }
+//                 })
+//             }
+//         });
+//     };
+
+//     function playGame() {
+//         console.log("We are going to start playing the game");
+//     //    inquirer.prompt([{
+//     //     type: 'confirm',
+//     //     message: "Do you want to create a new flashcard?",
+//     //     name: "makeAFlashCard"
+//     //     // body...
+
+var clozeCard = function(text, cloze, partText) {
     this.text = text;
     this.cloze = cloze;
-    this.clozeStatement = "[president_name] was the [president_number] president of the United States";
+    // this.partText = 
+    // this.clozeSelection = clozeSelection;
+    //     //refered to w3 schools
+    var newStatement = statement.replace(cloze, "_____________");
 
-    var president_names = [];
-    var president_number = [];
-    var loop = 0;
+    function partialText(cloze) {
+        // body...
+    }
 
-    var getPresident = function(loop) {
-        console.log(loop);
-        console.log(president_name);
 
-        if (loop < 2) {
-            inquirer.prompt({
-                name: "president_name",
-                message: "Enter a Presidents Name: "
-            }).then(function(answers) {
-                president_names.push(anwers.president_name);
-                loop++
-                getPresident();
-            });
-        }
-        getPresident();
+    function seeFrontOfCard() {
+        console.log(this.text)
+    }
+
+    function seeBackOfCard() {
+        console.log(this.cloze);
+
+
     }
 }
 
 
-
-
-
-
-//   this.front = function() {
-//     if (this.front === this.text) {
-//       console.log("That was yummy!");
-//       this.hungry = false;
-//       this.sleepy = true;
-//     }
-//     else {
-//       console.log("No thanks, I'm full.");
-//     }
-//   };
-
-//    {
-//     //not inside so we need to reference th
-//     console.log("THIS IS THE CLOZE TEXT:  " + this.text);
-
-//     var newText = this.text.replace(this.cloze, "_____________");
-//     console.log(newText);
-//     }
-
-//     this.seeBackOfCard = function() {
-//     console.log("THIS IS THE CLOZE ANSWER" + this.cloze);
-//     };
-// };
-
-
-
-// // var firstPresidentCloze = new ClozeCard(
-// //     "George Washington was the first president of the United States.", "George Washington");
-
-
-
-
-
-// var count = 0;
-// var newArr = [];
-
-// // var newFlashCards = function() {
-
-//     // if (count < 2) {
-//     //     // for (var i = 0; i<)
-//         inquirer.prompt([
-//           {
-//             name: "name",
-//             message: "what is your name?"
-//         }, {
-//             title: "title",
-//             message: "what is your title?"
-//         }, {
-//             age: "age",
-//             message: "how old are you?"
-//         }, {
-//             favoriteLanguage: "language",
-//             message: "What is your favorite language?"
-
-//         }]).then(function(answers) {
-//             console.log(answers)
-//             var newFlashCards = new ClozeCards(answers.text, answers.cloze);
-//             console.log(newFlashCards);
-//           });
-
-
-//       if (count.length < 5) {
-//   starters.push(player);
-//   team.push(player);
-//   console.log(player.name + " added to the starters");
-// }
-// else {
-//   subs.push(player);
-//   team.push(player);
-//   console.log(player.name + " added to the subs");
-// }
-//       newArr.push(newFlashCards);
-
-//       newFlashCards.seeFrontOfCard();
-
-//       count++;
-
-//       newFlashCards();
-//   });
-
-
-// console.log('All questions asked');
-
-// };
-
-// console.log(newFlashCards);
-
-
-// // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
-// var BrokenCloze = new ClozeCard("This doesn't work", "oops");
-
-
-// ClozeCard.prototype.seeFrontOfCard = function() {
-//     //not inside so we need to reference th
-//     console.log(this.text);
-//     var newText = this.text.replace(this.cloze, "_____________");
-//     console.log(newText);
-// }
-
-// ClozeCard.prototype.seeBackOfCard = function() {
-//     console.log(this.clozeSelection);
-// }
-
-// var question1 = new ClozeCard("George Washington was the first president of the United States.", "George Washington")
-
-
-// if (userInput == "yes") {
-//     PresidentQuestion.seeFrontOfCard();
-//     PresidentQuestion.seeBackOfCard();
-// }
-
-
-
-// function makeABasicFlashCard(front, back) {
-//     // blah blah blah do something with front
-//     // blah blah blah do something with back
-//     // fs.write("textfilename", this.front + this.back)
-//     makeAClozeCard();
-// }
-
-// function makeAClozeCard(front, back) {
-//     // blah blah blah do something with front
-//     // blah blah blah do something with back
-//     // fs.write("someothertextfilename", this.front + this.back)
-//     greeting();
-// }
-
-// // module.exports = ClozeCard;
+// //new constructor based on the first constructor function.  Note there are preset arguments
+// var playCards = new clozeCard("Who was the first president of the United States?", "George Washington");
